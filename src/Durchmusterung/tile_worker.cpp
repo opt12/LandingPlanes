0e75efd6c9d97dbb6e2b3e0181382883bda21ac1
Björn Wittich <bjoern_Wittich@gmx.de>
2017-06-10 22:32:24 +0200

    introduced tile_worker (for later threading)

---
+++ src/Durchmusterung/tile_worker.cpp
@@ -0,0 +1,11 @@
+#include "tile_worker.h"
+
+
+/*! \brief constructor of tile_manager
+ *   
+ *
+ * 
+ */
+tile_worker::tile_worker()
+{
+}
---
+++ src/Durchmusterung/tile_worker.h
@@ -0,0 +1,8 @@
+class tile_worker{
+
+  private:
+    
+  public:
+    tile_worker();
+
+};
