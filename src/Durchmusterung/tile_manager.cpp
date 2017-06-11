24ce9809b886ba1714fa4d22bd82abc695684b55
Björn Wittich <bjoern_Wittich@gmx.de>
2017-06-11 19:11:31 +0200

    added doxystyle documentation (not complete)

--- src/Durchmusterung/tile_manager.cpp
+++ src/Durchmusterung/tile_manager.cpp
@@ -4,24 +4,39 @@
 /*! \brief constructor of tile_manager
  *   
  *
- * 
+ * This constructors sets all class members to default 
  */
 tile_manager::tile_manager()
 {
  tiff_input_file="";
 }
 
+
+/*! \brief constructor of tile_manager
+ *    
+ *
+ * This constructors sets all class members to default and already predefines the input tiff file
+ */
 tile_manager::tile_manager(string input_file)
 {
   tiff_input_file=input_file;
 }
 
-void tile_manager::get_tile(tile_worker &worker_in,int xmin, int ymin, int width, int length)
+/*! \brief read from tiff to tile_worker
+ *    
+ *
+ * This method reads in a tile from a tiff file to the internal strcutre of the tile_worker
+ */
+void tile_manager::get_tile(tile_worker &worker_in, int xmin, int ymin, int width, int length)
 {
-  get_tile_array(worker_in,xmin,ymin,width,length);
+  get_tile_array(worker_in, xmin, ymin, width, length);
 }
 
-
+/*! \brief reads in tile 
+ *    
+ *
+ * This method reads in a tile defined by min x and miny and the dimensions width (x dimension) and length (y dimension). Pointer to extraction parameters and tile characteristics are given to the requesting tile_worker instance and in addition stored to the tile_manager global_map, so that additional workers can reuse the occupied memory (read only).
+ */
 void tile_manager::get_tile_array(tile_worker &worker_in, int xmin, int ymin, int width, int length)
 {
   tileCharacteristics *info;
