e6cea1e7d4518fe25ad61704efb9cd6a86b5717c
Björn Wittich <bjoern_Wittich@gmx.de>
2017-08-16 19:03:47 +0200

    remove dummy length

--- src/Durchmusterung/src/plane_library.cpp
+++ src/Durchmusterung/src/plane_library.cpp
@@ -9,9 +9,9 @@ int search_for_planes(const tileData *actualTile, GeoTiffHandler *myGeoTiffHandl
   double *not_defined = new double();
   *not_defined= noDataValue ;
   double orthogonal_slope = 3.0;
-  minLength=3000;
+  minLength=minLength;
   sem_t semaphore;
-   sem_init(&semaphore,0,16);
+  sem_init(&semaphore,0,16);
   cout << "before worker init"<<endl;
   tile_worker *worker1 = new tile_worker(actualTile, minLength, short_range_slope, long_range_slope, not_defined, heading, myGeoTiffHandler, width, orthogonal_slope, commSocket,taskDescription,&semaphore,pixelSize );
   cout << "before durchmustere"<<endl;
