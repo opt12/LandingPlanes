36db9d3ff974f4e94f77d280b1a53e4119522ebf
Felix <github@embedded-engineering.de>
2017-08-30 18:33:05 +0200

    Default Ordner wieder entfernt

--- src/1597_searchEngineWrapper/Default/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS := ../../Durchmusterung/lib/plane_library.a
-
-LIBS := -lpthread -ltiff -lgdal
-
