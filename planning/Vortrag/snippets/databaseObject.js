0a9ae8f507e12c29a6376d8e81e0b4fb453b8303
Felix <github@embedded-engineering.de>
2017-09-04 21:49:23 +0200

    - Vortragsskelett für LaTeX-Beamer erstellt und mit grober Kapitelstruktur erstellt

---
+++ planning/Vortrag/snippets/databaseObject.js
@@ -0,0 +1,47 @@
+{
+ "_id" : ObjectId("59a5afe2cde2b23ffe6b168f"),
+ "geoJSON" : {
+     "type" : "Feature",
+     "properties" : {
+         "actualHeading" : 90,
+         "actualLength" : 2214,
+         "actualRise" : 0.200002670288086,
+         "actualVariance" : 0.0065871326117382,
+         "maxRise" : 5,
+         "maxVariance" : 2.1,
+         "mergeable" : true,
+         "minLength" : 2000,
+         "minWidth" : 30,
+         "mergePass" : false,
+         "isMergeResult" : false
+     },
+     "geometry" : {
+         "type" : "Polygon",
+         "coordinates" : [ 
+             [ 
+                 [ 
+                     6.54887406221377, 
+                     51.3790183850639
+                 ], 
+                 [ 
+                     6.54885965890763, 
+                     51.3792878954276
+                 ], 
+                 [ 
+                     6.51699108255441, 
+                     51.3786165531896
+                 ], 
+                 [ 
+                     6.51700567265951, 
+                     51.37834704926
+                 ], 
+                 [ 
+                     6.54887406221377, 
+                     51.3790183850639
+                 ]
+             ]
+         ]
+     }
+ },
+ "__v" : 0
+}
