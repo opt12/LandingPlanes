726a1ee919b971dff4a89a35d9b1941cb1280d91
bjoern <bjoern@debianvm.fritz.box>
2017-10-01 07:41:07 +0200

    docu improved

--- src/Durchmusterung/src/plane_library.cpp
+++ src/Durchmusterung/src/plane_library.cpp
@@ -1,7 +1,7 @@
 #include "tile_worker.h"
 #include "global.h"
 
-int search_for_planes(const tileData* actualTile, GeoTiffHandler* myGeoTiffHandler, float heading, float minLength, float width, int commSocket, const json* taskDescription, float noDataValue, rectSize pixelSize, double short_range_slope, double long_range_slope, double orthogonal_slope, int numThreads )
+int search_for_planes(const tileData* actualTile /** [in] pointer to tile object including linear array of geo values  */, GeoTiffHandler* myGeoTiffHandler /** [in] pointer to GeoTiffHandler for performing GeoTiff translations  */, float heading /** [in] angle for plane orientation */, float minLength /** [in] minimum length of planes */, float width /** [in] desired width or plane */, int commSocket /** [out] socket for MongoDB  */, const json* taskDescription /** [in] task description json object for identification purpose in MongoDB */, float noDataValue /** [in] definition of the not defined value  */ , rectSize pixelSize /** [in] scaling information on pixel densitiy  */, double short_range_slope /** [in] max allowed slope between neighbour tiles */, double long_range_slope /** [in] max allowed slope between starting and endpoint of plane */, double orthogonal_slope /** [in] maximum allowed slope in orthogonal direction of neighboured tiles */, int numThreads /** [in] number of threads that should be used */)
 {
     double* not_defined = new double();
     *not_defined = noDataValue ;
