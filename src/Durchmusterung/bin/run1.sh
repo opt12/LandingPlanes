a9bbd044c568c3a385cb6bdc6ae474f344ca8edb
bjoern <bjoern@debianvm.fritz.box>
2017-10-02 07:34:36 +0200

    test file enhanced

--- src/Durchmusterung/bin/run1.sh
+++ src/Durchmusterung/bin/run1.sh
@@ -1,1 +1,1 @@
-./landebahn -E /home/bjoern/Downloads/DTM\ Germany_Nordrhein-Westfalen\,\ 20m.tif -L 2000 -A 30 >log 
+./landebahn -E /home/bjoern/Downloads/DTM\ Germany_Nordrhein-Westfalen\,\ 20m.tif -L 800 -A 0 -S 5 -T 5 -W 60 -O 5 -N 1 >log 
